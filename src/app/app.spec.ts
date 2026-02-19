e7b70ccf96c5030ef6198deed06880c64d3650c5
R4r4s <rngheru@gmail.com>
2026-02-19 18:13:28 +0100

    feat(header): implement header component with responsive design
    - Added header HTML structure with navigation links and a call-to-action button.
    - Created SCSS styles for the header, including responsive adjustments for mobile devices.
    - Defined color and typography variables in a new SCSS variables file for consistent styling across the application.
    - Implemented the header component in Angular with standalone configuration.

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, front-client');
+    expect(compiled.querySelector('.home-intro__title')?.textContent).toContain('clicTec');
   });
 });
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
-  protected readonly title = signal('front-client');
-}
+export class App {}
