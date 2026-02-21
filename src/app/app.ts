2c9d20f7d607b5164da0a5b83feacf80d413ab83
R4r4s <rngheru@gmail.com>
2026-02-21 21:09:05 +0100

    feat(footer): agregar componente de pie de página con contenido y estilos, mejorar accesibilidad feat(header): corregir textos y mejorar accesibilidad en la navegación feat(inicio): corregir errores tipográficos y mejorar la redacción en la página de inicio feat(app): agregar componente de pie de página en la estructura principal de la aplicación

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './shared/components/footer/footer';
 import { HeaderComponent } from './shared/components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, RouterOutlet],
+  imports: [HeaderComponent, RouterOutlet, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
