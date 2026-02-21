c486adea569b2c2b4e85be8f696a269c2f67dbed
R4r4s <rngheru@gmail.com>
2026-02-21 13:34:51 +0100

    feat(inicio): agregar sección de héroe con botones y estadísticas, actualizar estilos y fuentes

--- src/app/pages/inicio/inicio-page.ts
+++ src/app/pages/inicio/inicio-page.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-inicio-page',
   standalone: true,
+  imports: [RouterLink],
   templateUrl: './inicio-page.html',
   styleUrl: './inicio-page.scss'
 })
